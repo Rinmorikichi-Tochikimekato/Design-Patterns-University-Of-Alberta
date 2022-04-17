1fc2f3c83e3689b4055a990bf4c930188254eb4a
Mohit Nirwan <40948557+Rinmorikichi-Tochikimekato@users.noreply.github.com>
2022-04-17 14:11:50 +0530

    Create OldCoffeeMachine.java

---
+++ Week 1/Adapter Pattern Ungraded Assignment/OldCoffeeMachine.java
@@ -0,0 +1,11 @@
+public class OldCoffeeMachine {
+    
+    public void chooseFirstSelection(){
+        System.out.println("Fetching coffee 1");
+    }
+    
+    public void chooseSecondSelection(){
+        System.out.println("Fetching coffee 2");
+    }
+
+}
